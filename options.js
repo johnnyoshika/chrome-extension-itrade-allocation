fcd91abdc1c3078f2068095d53bd8de67a34ef82
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-08 23:53:51 -0800

    Edit and store conversion from options page

--- options.js
+++ options.js
@@ -1,6 +1,8 @@
 var noMappingsTemplate = _.template(document.querySelector('#no-mappings').innerHTML);
 var mappingsTemplate = _.template(document.querySelector('#mappings').innerHTML);
 var mapTemplate = _.template(document.querySelector('#map').innerHTML);
+var conversionTemplate = _.template(document.querySelector('#conversion').innerHTML);
+var conversionEditTemplate = _.template(document.querySelector('#conversion-edit').innerHTML);
 
 var renderMappings = mappings =>
   mappings.length
@@ -12,11 +14,23 @@ var renderMap = () => {
   document.querySelector('input').focus();
 }
 
+var renderConversion = conversion =>
+  document.querySelector('[data-outlet="conversion"]').innerHTML = conversionTemplate({conversion: conversion});
+
+var renderEditConversion = () =>
+  document.querySelector('[data-outlet="conversion-edit"]').innerHTML = conversionEditTemplate({conversion: ''});
+
 chrome.storage.sync.get('mappings', data =>
   (data.mappings = data.mappings || []) && renderMappings(data.mappings));
 
+chrome.storage.sync.get('conversion', data =>
+  (data.conversion = data.conversion || 1) && renderConversion(data.conversion));
+
 chrome.storage.onChanged.addListener((changes, namespace) =>
   changes.mappings && renderMappings(changes.mappings.newValue));
+
+chrome.storage.onChanged.addListener((changes, namespace) =>
+  changes.conversion && renderConversion(changes.conversion.newValue));
 
 document.querySelector('[data-outlet="map"]').addEventListener('keydown', e => {
   if (e.defaultPrevented) return; // Should do nothing if the default action has been cancelled
@@ -56,4 +70,15 @@ document.querySelector('[data-outlet="mappings"]').addEventListener('click', e =
   });
 });
 
+document.querySelector('[data-outlet="conversion-edit"]').addEventListener('keydown', e => {
+  if (e.defaultPrevented) return; // Should do nothing if the default action has been cancelled
+  if(e.target.tagName !== 'INPUT') return;
+  if (e.keyCode !== 13) return;
+
+  e.preventDefault();
+  var conversion = document.querySelector('input[name="conversion"]').value;
+  chrome.storage.sync.set({conversion: parseFloat(conversion)}, () => document.querySelector('input[name="conversion"]').value = '');
+});
+
 renderMap();
+renderEditConversion();
