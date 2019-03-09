268dee01f714365457ca9d5f919fb1057f5ae7c7
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-08 22:37:11 -0800

    Wire add and reset in popup

--- popup.js
+++ popup.js
@@ -1,22 +1,34 @@
 var noPositionsTemplate = _.template(document.querySelector('#no-positions').innerHTML);
 var positionsTemplate = _.template(document.querySelector('#positions').innerHTML);
+var positions;
 
 chrome.tabs.query({active: true, currentWindow: true}, tabs =>
   chrome.tabs.executeScript(tabs[0].id, { file: 'contentScript.js' }));
 
-chrome.runtime.onMessage.addListener((request) => {
-  if (!request.positions) return;
+var renderPortfolio = (type, positions) =>
+  positions.length
+    ? document.querySelector(`[data-outlet="${type}"]`).innerHTML = positionsTemplate({ positions: positions })
+    : document.querySelector(`[data-outlet="${type}"]`).innerHTML = noPositionsTemplate();
 
-  if (!request.positions.length)
-    document.querySelector('[data-outlet="account"]').innerHTML = noPositionsTemplate();
-  else
-    document.querySelector('[data-outlet="account"]').innerHTML = positionsTemplate({ positions: request.positions });
+chrome.runtime.onMessage.addListener(request => {
+  positions = request.positions;
+  request.positions && renderPortfolio('account', request.positions)
 });
 
+chrome.storage.sync.get('positions', data => 
+  (data.positions = data.positions || []) && renderPortfolio('total', data.positions));
+
+chrome.storage.onChanged.addListener((changes, namespace) =>
+  changes.positions && renderPortfolio('total', changes.positions.newValue));
+
+
 document.querySelector('#add').addEventListener('click', e => {
-  console.log('clicked add');
+  chrome.storage.sync.get('positions', data => {
+    data.positions = data.positions || [];
+    chrome.storage.sync.set({positions: data.positions.concat(positions)});
+  });
 }, false);
 
 document.querySelector('#reset').addEventListener('click', e => {
-  console.log('clicked reset');
+  chrome.storage.sync.set({positions: []});
 }, false);
