10296f1d4559902083116b2c49c3508fdd2dd0ad
Johnny Oshika <johnnyoshika@gmail.com>
2019-03-26 22:56:00 -0700

    Support negative cash balance in Questrade

--- src/contents/questrade-balances.js
+++ src/contents/questrade-balances.js
@@ -1,9 +1,9 @@
 (function () {
+    let negate = text => (text && /\([\d,$\.]+\)/.test(text)) ? -1 : 1;
+    let parseValue = text => text && (negate(text) * parseFloat(text.replace(/[,$()]/g, '')));
 
-    let parseValue = text => text && parseFloat(text.replace(/[,$]/g, ''));
-
-    let cad = parseValue($('[data-qt="lblCash_BALANCES_CAD"]').text());
-    let usd = parseValue($('[data-qt="lblCash_BALANCES_USD"]').text());
+    let cad = parseValue($('[data-qt="lblCash_BALANCES_CAD"]').find('.value').text());
+    let usd = parseValue($('[data-qt="lblCash_BALANCES_USD"]').find('.value').text());
     let accountName = $('[data-qt="lblSelectorName"]').first().text();
 
     if (!accountName)
